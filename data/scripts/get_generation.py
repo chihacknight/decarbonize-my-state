0d488263cc11474962446c3eed63923b10c7163e
Sean Watland <51850964+swatland610@users.noreply.github.com>
2022-02-09 09:39:31 -0600

    replace read csv with input1 for makefile

--- data/scripts/get_generation.py
+++ data/scripts/get_generation.py
@@ -6,7 +6,7 @@ from utils import group_df_by_generation
 if __name__ == '__main__':
     input1 = sys.argv[1]
 
-    generation_data = pd.read_csv('../raw/us_electric_generation_2001_20.csv')
+    generation_data = pd.read_csv(input1)
     generation_out = group_df_by_generation(generation_data)
     
     print(generation_out.to_json())
