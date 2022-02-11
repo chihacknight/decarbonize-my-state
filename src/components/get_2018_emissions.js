036e3da4896b11b52608a9a588103c05c0acfb71
Derek Eder <derek.eder@gmail.com>
2022-02-11 10:50:49 -0600

    using lowercase state keys, removed patch convertslug function

--- src/components/get_2018_emissions.js
+++ src/components/get_2018_emissions.js
@@ -3,7 +3,7 @@ export default function get_2018_emissions (data) {
   var emissions_2018 = {}
   for (var key of Object.keys(data.finalJson)) {
     var year_data = data.finalJson[key].filter(v => v.year == 2018)[0]
-    if (key != "United_States")
+    if (key != "united_states")
       emissions_2018[key] = Math.round(year_data.dirty_power + year_data.buildings + year_data.transportation + year_data.dumps_farms_industrial)
   }
   return emissions_2018
