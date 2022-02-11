ff93545641707aa964d5f66d41ad9948300282d5
Derek Eder <derek.eder@gmail.com>
2022-02-10 22:39:09 -0600

    we have a map!

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,26 +6,20 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ChoroplethMap from "../components/choroplethmap"
 import stateValues from "../components/statevalues"
-import occupation_json from '../../static/initial_occupations.json'
 
 
 const IndexPage = ({data}) => {
-  // filter out occupations that have no state data
-  const occupations = data.allOccupationsJson.nodes.filter((occ) => {
-    const allValues = stateValues(occ)
-    const allNums = allValues.filter(v => typeof v !== "string")
-    return allNums.some(num => typeof num === "number")
-  })
 
-  const [occupation,setOccupation] = useState(null)
-  
-  useEffect(() => {
-    const homeOccupations = occupations.filter(occupation =>
-      occupation_json.occupations.includes(occupation.occupation_slug))
+  // console.log(data.finalJson)
 
-    const randomIndex = Math.floor(Math.random() * homeOccupations.length)
-    setOccupation(homeOccupations[randomIndex])
-  },[])
+  var emissions_2018 = {}
+  for (var key of Object.keys(data.finalJson)) {
+    var year_data = data.finalJson[key].filter(v => v.year == 2018)[0]
+    if (key != "United_States")
+      emissions_2018[key] = Math.round(year_data.dirty_power + year_data.buildings + year_data.transportation + year_data.dumps_farms_industrial)
+  }
+
+  console.log(emissions_2018)
 
   return (
     <Layout>
@@ -37,33 +31,8 @@ const IndexPage = ({data}) => {
         </strong>
       </h3><br />
 
-      <Typeahead
-        clearButton
-        highlightOnlyResult
-        selected={occupations.filter(o => {
-          if (occupation) {
-            return o.occupation_slug === occupation.occupation_slug
-          } else {
-            return null
-          }
-        })}
-        id="map-occupation"
-        placeholder="Search by occupation ..."
-        options={occupations}
-        labelKey={(option) => `${option.occupation}`}
-        size="large"
-        onChange={(option) => {
-          if (option !== undefined) {
-            if (option.length === 0) {
-              setOccupation(null)
-            } else {
-              setOccupation(option[0])
-            }
-          }
-        }}
-      />
       <p class='pt-4'></p>
-      <ChoroplethMap occupation={occupation} />
+      <ChoroplethMap emissions={emissions_2018} />
       <br /><br />
     </Layout>
   )
