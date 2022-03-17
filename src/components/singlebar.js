3a41682a4da5d5bd8b616550eb6387294759ea7c
Howard <howard@mmpix.com>
2022-03-17 18:39:47 -0500

    Update singlebar.js
    Correct Version of single bar

--- src/components/singlebar.js
+++ src/components/singlebar.js
@@ -1,84 +1,93 @@
-import React from 'react';
-import { BarChart, Bar,  XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import "./styles.css";
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  LabelList,
+  CartesianGrid
+} from "recharts";
 
-const barColors = [
-    "#984ea3",
-    "#4daf4a",
-    "#377eb8",
-    "#e41a1c"
-  ]
-  
-const data = [
+const barColors = ["#685e4a", "#b7b7b7", "#d9d9d9", "#f3f3f3"];
+
+/*Sample emissions_data to show data structure
+componsent is currently written for the specific case and not
+the general case.  Data MUST be in this format!
+*/
+const emissions_data = [
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+    buildings: [50.2, "Buildings"],
+    dirty_power: [34.7, "DirtyPower"],
+    dumps_farms_industrial_other: [141.5, "Dumps, Farms, Other"],
+    transportation: [50.3, "Transportation"]
+  }
 ];
 
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/stacked-bar-chart-s47i2';
+// Determine total and category percentages
+var total = emissions_data[0].buildings[0] + emissions_data[0].dirty_power[0];
+total =
+  total +
+  emissions_data[0].dumps_farms_industrial_other[0] +
+  emissions_data[0].transportation[0];
 
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-          <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
-    );
-  }
+emissions_data[0].buildings[1] = emissions_data[0].buildings[1] + " " +
+  Math.round((100 * emissions_data[0].buildings[0]) / total) + "%";
+
+emissions_data[0].dirty_power[1] = emissions_data[0].dirty_power[1] + " " +
+  Math.round((100 * emissions_data[0].dirty_power[0]) / total) + "%";
+
+emissions_data[0].dumps_farms_industrial_other[1] = emissions_data[0].dumps_farms_industrial_other[1] + " " +
+  Math.round((100 * emissions_data[0].dumps_farms_industrial_other[0]) / total) + "%";
+
+emissions_data[0].transportation[1] = emissions_data[0].transportation[1] + " " +
+  Math.round((100 * emissions_data[0].transportation[0]) / total) + "%";
+
+//Render the stacked bar.
+  export default function App() {
+  return (
+    <BarChart
+      barSize={250}
+      width={500}
+      height={300}
+      data={emissions_data}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="0 99" />
+      <XAxis dataKey=" " hide={true} />
+      <YAxis
+        axisLine={false}
+        tickLine={false}
+        label={{
+          angle: -90,
+          value: "CO2 (million metric tons)",
+          position: "insideBottomLeft"
+        }}
+      />
+      <Bar
+        dataKey="dumps_farms_industrial_other[0]"
+        stackId="a"
+        fill={barColors[0]}
+      >
+        <LabelList
+          dataKey="dumps_farms_industrial_other[1]"
+          position="insideTopLeft"
+        />
+      </Bar>
+      <Bar dataKey="transportation[0]" stackId="a" fill={barColors[1]}>
+        <LabelList dataKey="transportation[1]" position="insideTopLeft" />
+      </Bar>
+      <Bar dataKey="buildings[0]" stackId="a" fill={barColors[2]}>
+        <LabelList dataKey="buildings[1]" position="insideTopLeft" />
+      </Bar>
+      <Bar dataKey="dirty_power[0]" stackId="a" fill={barColors[3]}>
+        <LabelList dataKey="dirty_power[1]" position="insideTopLeft" />
+      </Bar>
+    </BarChart>
+  );
 }
