ee33269cc919942829d8eadccb86aba6f717d2f3
Derek Eder <derek.eder@gmail.com>
2022-03-22 22:09:17 -0500

    cleanup the single bar component

--- src/components/singlebar.js
+++ src/components/singlebar.js
@@ -1,50 +1,23 @@
 import React from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  LabelList,
-  CartesianGrid
-} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, LabelList, CartesianGrid } from "recharts"
 
-const barColors = ["#685e4a", "#b7b7b7", "#d9d9d9", "#f3f3f3"];
+const getLabel=(props, label) => { return label }
 
-function getTotal (payload) {
-  return payload[0].value + payload[1].value + payload[2].value + payload[3].value
-}
-
-function calcPercent (payload, index) {
-  return (100 * payload[index].value.toFixed(1) / getTotal(payload)).toFixed(1)
-}
-
-// const renderLabel = (props, text) =>{
-//   const { x, y, width, height, value } = props
-//   return (
-//     <span>{text}: {value}</span>
-//     )
-// }
-
-const getLabel=(props, label) => {return  label}
-
-
-//Render the stacked bar.
-  export default function SingleBarChart({emissions_data}) {
+export default function SingleBarChart({emissions_data}) {
   return (
     <BarChart
       barSize={250}
       width={500}
-      height={300}
+      height={600}
       data={emissions_data}
       margin={{
-        top: 20,
+        top: 0,
         right: 30,
         left: 20,
         bottom: 5
       }}
     >
       <CartesianGrid strokeDasharray="0 99" />
-      <XAxis dataKey=" " hide={true} />
       <YAxis
         axisLine={false}
         tickLine={false}
@@ -54,27 +27,18 @@ const getLabel=(props, label) => {return  label}
           position: "insideBottomLeft"
         }}
       />
-      <Bar
-        dataKey="dumps_farms_industrial_other"
-        stackId="a"
-        fill={barColors[0]}
-      >
-         <LabelList
-          content={props => getLabel(props, 'Dumps, Farms, Industrial & Other')}
-          position="insideTopLeft"
-        /> 
+      <Bar dataKey="dumps_farms_industrial_other" stackId="a" fill="#98886c">
+        <LabelList content={props => getLabel(props, 'Dumps, Farms, Industrial & Other')} position="insideTopLeft" /> 
       </Bar>
-     <Bar dataKey="transportation" stackId="a" fill={barColors[1]}>
-        <LabelList 
-        content={props => getLabel(props, 'Transportation')}
-        position="insideTopLeft" /> 
+     <Bar dataKey="transportation" stackId="a" fill="#b7b7b7">
+        <LabelList content={props => getLabel(props, 'Transportation')} position="insideTopLeft" /> 
       </Bar>
-       <Bar dataKey="buildings" stackId="a" fill={barColors[2]}>
+      <Bar dataKey="buildings" stackId="a" fill="#d9d9d9">
         <LabelList content={props => getLabel(props, 'Buildings')} position="insideTopLeft" />
       </Bar>
-      <Bar dataKey="dirty_power" stackId="a" fill={barColors[3]}>
+      <Bar dataKey="dirty_power" stackId="a" fill="#f3f3f3">
         <LabelList content={props => getLabel(props, 'Dirty Power')} position="insideTopLeft" />
       </Bar>
     </BarChart>
-  );
+  )
 }
