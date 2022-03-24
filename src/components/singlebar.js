7930553046eb3501f6eb10d94869f38963f1b0b3
Dylan <dhalpern@uchicago.edu>
2022-03-23 19:44:03 -0500

    Content change to valueAccessor on label list

--- src/components/singlebar.js
+++ src/components/singlebar.js
@@ -1,9 +1,15 @@
 import React from "react"
 import { BarChart, Bar, XAxis, YAxis, LabelList, CartesianGrid } from "recharts"
 
-const getLabel=(props, label) => { return label }
+const getPct = (value, total) => { return Math.round((value/total)*100) }
+const getLabel = (entry, total, field, label) => { return `${label}: ${getPct(entry[field], total)}%` }
 
 export default function SingleBarChart ({emissions_data}) {
+  // sum all emissions fields except year
+  const totalEmissions = Object.entries(emissions_data[0])
+    .filter(([key,_val]) => key !== 'year')
+    .reduce((acc, [_key,val]) => acc + val, 0);
+
   return (
     <BarChart
       barSize={250}
@@ -28,16 +34,16 @@ export default function SingleBarChart ({emissions_data}) {
         }}
       />
       <Bar dataKey="dumps_farms_industrial_other" stackId="a" fill="#98886c">
-        <LabelList content={props => getLabel(props, 'Dumps, Farms, Industrial & Other')} position="insideTopLeft" /> 
+        <LabelList valueAccessor={entry => getLabel(entry, totalEmissions, 'dumps_farms_industrial_other', 'Dumps, Farms, Industrial & Other')} position="insideTopLeft" /> 
       </Bar>
       <Bar dataKey="transportation" stackId="a" fill="#b7b7b7">
-        <LabelList content={props => getLabel(props, 'Transportation')} position="insideTopLeft" /> 
+        <LabelList valueAccessor={entry => getLabel(entry, totalEmissions, 'transportation', 'Transportation')} position="insideTopLeft" /> 
       </Bar>
       <Bar dataKey="buildings" stackId="a" fill="#d9d9d9">
-        <LabelList content={props => getLabel(props, 'Buildings')} position="insideTopLeft" />
+        <LabelList valueAccessor={entry => getLabel(entry, totalEmissions, 'buildings', 'Buildings')} position="insideTopLeft" />
       </Bar>
       <Bar dataKey="dirty_power" stackId="a" fill="#f3f3f3">
-        <LabelList content={props => getLabel(props, 'Dirty Power')} position="insideTopLeft" />
+        <LabelList valueAccessor={entry => getLabel(entry, totalEmissions, 'dirty_power', 'Dirty Power')} position="insideTopLeft" />
       </Bar>
     </BarChart>
   )
