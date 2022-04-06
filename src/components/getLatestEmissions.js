a14188f90efe7851d9610d950352f834a5c4daf0
Dylan <dhalpern@uchicago.edu>
2022-04-05 23:04:06 -0500

    Cleanup Emissions functions

--- src/components/getLatestEmissions.js
+++ src/components/getLatestEmissions.js
@@ -1,16 +1,12 @@
 // unpack the us emissions data for 2018 and total up all 4 categories by state
 export function getLatestUsData (data) {
-  for (var key of Object.keys(data)) {
-    var year_data = data[key].emissionsByYear.filter(v => v.year === 2018)[0]
-    if (key !== "united_states") {
-      return year_data
-    }
-  }
-  return {}
+  const sortedYears = [...data[Object.keys(data)[0]].emissionsByYear].sort((a, b) => b.year - a.year)
+  const latestYear = sortedYears[0].year
+  return data['united_states'].emissionsByYear.filter(v => v.year === latestYear)
 }
 
 export function getLatestEmissions (data) {
-  const sortedYears = [...data[Object.keys(data)[0]].emissionsByYear].sort((a, b) => a.year - b.year)
+  const sortedYears = [...data[Object.keys(data)[0]].emissionsByYear].sort((a, b) => b.year - a.year)
   const latestYear = sortedYears[0].year
   let mutableDataObj = {}
 
