64122e71da6b01dec53b9247fbe932c0ca22cfb2
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2022-04-12 20:26:52 -0500

    Update layout to make state details graph sticky on desktop

--- src/components/state-details.js
+++ src/components/state-details.js
@@ -102,8 +102,6 @@ const StateDetailsPage = ({location, data}) => {
         </h1>
       </div>
 
-      {/* { !placeData.emissions } */}
-
       {/* Intro Section */}
       <div className='col-12'>
         <p className="h1 font-weight-light mt-6 mb-6">
@@ -118,225 +116,234 @@ const StateDetailsPage = ({location, data}) => {
         <p className="h6 text-muted">
           Metric tons of carbon dioxide equivalent (MTCO2e) emissions
         </p>
-  
+
         <p className="h1 font-weight-bold text-center mt-5">We can do it. Here's how.</p>
 
         <hr className="mt-5"/>
       </div>
 
-      {/* Buildings Section */}
-      <div className='col-12'>
-        <h2 className="h3 mt-5 font-weight-bold">Buildings</h2>
-
-        <p className="h3 mt-5">
-          <strong className="font-weight-bold">{buildingsPrcnt}%</strong> of
-          emissions in {placeTitle} comes from buildings.
-        </p>
-
-        <div className="row mt-5">
-          { /* Make SingleBarChart full width on mobile */ }
-          <div className="col-12-med">
-            <SingleBarChart
-              emissionsData={latestEmissions}
-              activeKey='buildings' />
-          </div>
-
-          <div className="col h3">
-            <p className="mt-5">
-              Mostly from heating them.
-            </p>
-
-            <p className="mt-5">
-              ?% of the pollution of your typical home comes from heating your
-              space, water, and food.
-            </p>
-          </div>
-        </div>
-
-        <p className="h3 mt-5">
-          To stop this pollution, we need to electrify our furnaces, water boilers, and stoves.
-        </p>
-
-        <p className="h3 mt-5">
-          And we need to do this for all {buildingsCountStr} buildings in {placeTitle} (That's around {buildingsPerYear} per year)
-        </p>
-
-        <p className="h3 mt-7 font-weight-bold text-center">
-          That will solve {buildingsPrcnt}% of the problem.
-        </p>
-
-        <div className="mt-5 d-flex justify-content-center">
+      <div className="row">
+        { /* Our main chart for desktop ONLY (others are hidden) */ }
+        <div className="col-4 sticky-cont d-none d-xl-block">
           <SingleBarChart
+            isSticky={true}
             emissionsData={latestEmissions}
             greenKeys={ [ 'buildings' ] } />
         </div>
+        {/* Right column on desktop, full width on mobile */}
+        <div className="col-12 col-xl-8">
+          {/* Buildings Section */}
+          <h2 className="h3 mt-5 font-weight-bold">Buildings</h2>
 
-        {/* TODO: Make these link somewhere */}
-        <ul className="mt-3 pl-4 mb-0">
-          <li>
-            <a href="http://example.com">First, electrify your building(s)</a>
-          </li>
-          <li>
-            <a href="http://example.com">
-                Then push your local politicians to electrify the rest
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className='col-12 col-lg-8'>
-          
+          <p className="h3 mt-5">
+            <strong className="font-weight-bold">{buildingsPrcnt}%</strong> of
+            emissions in {placeTitle} comes from buildings.
+          </p>
 
-        <hr className="mt-5"/>
-      </div>
-
-      {/* Transportation Section */}
-      <div className='col-12'>
-        <h2 className="h3 mt-5 font-weight-bold">Getting Around</h2>
-
-        <p className="h3 mt-5">
-          <strong className="font-weight-bold">{transportPrcnt}%</strong> of
-          emissions in {placeTitle} comes from cars, trucks, and planes.
-        </p>
-
-        <div className="row mt-5">
-          { /* Make SingleBarChart full width on mobile */ }
-          <div className="col-12-med">
+          <div className="row mt-5">
+          <div className="mt-5 d-flex justify-content-center d-block d-xl-none">
             <SingleBarChart
               emissionsData={latestEmissions}
-              activeKey='transportation' />
+              greenKeys={ [ 'buildings' ] } />
           </div>
 
-          <div className="col h3">
-            <p className="mt-5">
-              Mostly from our cars.
-            </p>
+            <div className="col h3">
+              <p className="mt-5">
+                Mostly from heating them.
+              </p>
 
-            <p className="mt-5">
-              To cut this pollution, replace your car with an EV.
-            </p>
-
-            <p className="mt-5">
-              And we need to do this for all {carsCountStr} cars 
-              in {placeTitle} (That's around {carsPerYear} a year. 
-              Excluding the {evCountStr} EVs already in {placeTitle})
-            </p>
+              <p className="mt-5">
+                ?% of the pollution of your typical home comes from heating your
+                space, water, and food.
+              </p>
+            </div>
           </div>
-        </div>
 
-        <p className="h3 mt-7 font-weight-bold text-center">
-          That will solve another {transportPrcnt}% of the problem.
-        </p>
+          <p className="h3 mt-5">
+            To stop this pollution, we need to electrify our furnaces, water boilers, and stoves.
+          </p>
 
-        <div className="mt-5 d-flex justify-content-center">
-          <SingleBarChart
-            emissionsData={latestEmissions}
-            greenKeys={[ 'buildings', 'transportation' ]} />
-        </div>
+          <p className="h3 mt-5">
+            And we need to do this for all {buildingsCountStr} buildings in {placeTitle} (That's around {buildingsPerYear} per year)
+          </p>
 
-        <div className="action-panel">
-          <h3 className="h4 font-weight-bold">What should I do?</h3>
+          <p className="h3 mt-7 font-weight-bold text-center">
+            That will solve {buildingsPrcnt}% of the problem.
+          </p>
 
-          {/* TODO: Make these link somewhere */}
-          <ul className="mt-3 pl-4 mb-0">
-            <li>
-              <a href="http://example.com">
-                If you have a car, buy an EV
-              </a>
-            </li>
-            <li>
-              <a href="http://example.com">
-                Then push your local politicians to electrify the rest
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <hr className="mt-5"/>
-      </div>
-
-      {/* Power Section */}
-      <div className='col-12'>
-        <h2 className="h3 mt-5 font-weight-bold">Power Generation</h2>
-
-
-        {
-          // Show special section if power emissions are zero
-          powerPrcnt === '0' &&
-          <div className="mt-8 mb-8 text-center">
-            <p className="h3 font-weight-bold">
-              {placeTitle} has no emissions from making power,
-              it's doing great! 😎
-            </p>
-
-            <p className="h5 mt-3">
-              Check out another state to see how they can cut their power
-              emissions to zero.
-            </p>
-          </div>
-        }
-        { powerPrcnt > 0 && <HowToCleanPowerSection
-          latestEmissions={latestEmissions}
-          placeTitle={placeTitle}
-          powerPrcnt={powerPrcnt} /> }
-
-        <hr className="mt-5"/>
-      </div>
-
-      {/* Other Section */}
-      <div className='col-12'>
-        <h2 className="h3 mt-5 font-weight-bold">Other Emissions</h2>
-
-        <p className="h3 mt-5">
-          The last <strong className="font-weight-bold">{otherPrcnt}%</strong> of
-          emissions in {placeTitle} comes other sources
-        </p>
-
-        <div className="row mt-5">
-          { /* Make SingleBarChart full width on mobile */ }
-          <div className="col-12-med">
+          <div className="mt-5 d-flex justify-content-center d-block d-xl-none">
             <SingleBarChart
               emissionsData={latestEmissions}
-              activeKey='dumps_farms_industrial_other' />
+              greenKeys={ [ 'buildings' ] } />
           </div>
 
-          <div className="col">
-            <p className="h3 mt-5">
-              This includes industry, landfills, and farming.
-            </p>
+          <div className="action-panel">
+            <h3 className="h4 font-weight-bold">What should I do?</h3>
 
-            <p className="mt-3">
-              There's no one solution to solve these problems, but there are a
-              lot of great ideas!
-            </p>
-
-            <p>
-              These include:
-            </p>
-
-            <ul>
-              <li>Regenerative agriculture to sequester carbon in soil</li>
-              <li>Composting to reduce landfill methane emissions</li>
+            {/* TODO: Make these link somewhere */}
+            <ul className="mt-3 pl-4 mb-0">
               <li>
-                New techniques for manufacturing
-                CO<sub>2</sub> emitting materials, like concrete
+                <a href="http://example.com">First, electrify your building(s)</a>
+              </li>
+              <li>
+                <a href="http://example.com">
+                    Then push your local politicians to electrify the rest
+                </a>
               </li>
             </ul>
           </div>
-        </div>
 
-        <hr className="mt-5" />
+          <hr className="mt-5"/>
 
-        <section className="text-center mb-8">
-          <div className="h1 mt-7 font-weight-bold">
-            And that's it! 🎉
+          {/* Transportation Section */}
+          <div className='col-12'>
+            <h2 className="h3 mt-5 font-weight-bold">Getting Around</h2>
+
+            <p className="h3 mt-5">
+              <strong className="font-weight-bold">{transportPrcnt}%</strong> of
+              emissions in {placeTitle} comes from cars, trucks, and planes.
+            </p>
+
+            <div className="row mt-5">
+              { /* Make SingleBarChart full width on mobile */ }
+              <div className="col-12-med d-block d-xl-none">
+                <SingleBarChart
+                  emissionsData={latestEmissions}
+                  activeKey='transportation' />
+              </div>
+
+              <div className="col h3">
+                <p className="mt-5">
+                  Mostly from our cars.
+                </p>
+
+                <p className="mt-5">
+                  To cut this pollution, replace your car with an EV.
+                </p>
+
+                <p className="mt-5">
+                  And we need to do this for all {carsCountStr} cars
+                  in {placeTitle} (That's around {carsPerYear} a year.
+                  Excluding the {evCountStr} EVs already in {placeTitle})
+                </p>
+              </div>
+            </div>
+
+            <p className="h3 mt-7 font-weight-bold text-center">
+              That will solve another {transportPrcnt}% of the problem.
+            </p>
+
+            <div className="mt-5 d-flex justify-content-center d-block d-xl-none">
+              <SingleBarChart
+                emissionsData={latestEmissions}
+                greenKeys={[ 'buildings', 'transportation' ]} />
+            </div>
+
+            <div className="action-panel">
+              <h3 className="h4 font-weight-bold">What should I do?</h3>
+
+              {/* TODO: Make these link somewhere */}
+              <ul className="mt-3 pl-4 mb-0">
+                <li>
+                  <a href="http://example.com">
+                    If you have a car, buy an EV
+                  </a>
+                </li>
+                <li>
+                  <a href="http://example.com">
+                    Then push your local politicians to electrify the rest
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <hr className="mt-5"/>
           </div>
 
-          <p className="h4 mt-4">
-           We hope this gives you some ideas for what you can do to get your state
-           to zero emissions!
-          </p>
-        </section>
+          {/* Power Section */}
+          <div className='col-12'>
+            <h2 className="h3 mt-5 font-weight-bold">Power Generation</h2>
+
+            {
+              // Show special section if power emissions are zero
+              powerPrcnt === '0' &&
+              <div className="mt-8 mb-8 text-center">
+                <p className="h3 font-weight-bold">
+                  {placeTitle} has no emissions from making power,
+                  it's doing great! 😎
+                </p>
+
+                <p className="h5 mt-3">
+                  Check out another state to see how they can cut their power
+                  emissions to zero.
+                </p>
+              </div>
+            }
+            { powerPrcnt > 0 && <HowToCleanPowerSection
+              latestEmissions={latestEmissions}
+              placeTitle={placeTitle}
+              powerPrcnt={powerPrcnt} /> }
+
+            <hr className="mt-5"/>
+          </div>
+
+          {/* Other Section */}
+          <div className='col-12'>
+            <h2 className="h3 mt-5 font-weight-bold">Other Emissions</h2>
+
+            <p className="h3 mt-5">
+              The last <strong className="font-weight-bold">{otherPrcnt}%</strong> of
+              emissions in {placeTitle} comes other sources
+            </p>
+
+            <div className="row mt-5">
+              { /* Make SingleBarChart full width on mobile */ }
+              <div className="col-12-med d-block d-xl-none">
+                <SingleBarChart
+                  emissionsData={latestEmissions}
+                  activeKey='dumps_farms_industrial_other' />
+              </div>
+
+              <div className="col">
+                <p className="h3 mt-5">
+                  This includes industry, landfills, and farming.
+                </p>
+
+                <p className="mt-3">
+                  There's no one solution to solve these problems, but there are a
+                  lot of great ideas!
+                </p>
+
+                <p>
+                  These include:
+                </p>
+
+                <ul>
+                  <li>Regenerative agriculture to sequester carbon in soil</li>
+                  <li>Composting to reduce landfill methane emissions</li>
+                  <li>
+                    New techniques for manufacturing
+                    CO<sub>2</sub> emitting materials, like concrete
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <hr className="mt-7" />
+
+      <section className="text-center mb-8">
+        <div className="h1 mt-7 font-weight-bold">
+          And that's it! 🎉
+        </div>
+
+        <p className="h4 mt-4">
+         We hope this gives you some ideas for what you <br className="d-none d-lg-block"/>
+         can do to get your state to zero emissions!
+        </p>
+      </section>
     </Layout>
   )
 }
@@ -359,7 +366,7 @@ function HowToCleanPowerSection ({
 
       <div className="row mt-5">
         { /* Make SingleBarChart full width on mobile */ }
-        <div className="col-12-med">
+        <div className="col-12-med d-block d-xl-none">
           <SingleBarChart
             emissionsData={latestEmissions}
             activeKey='dirty_power' />
@@ -419,7 +426,7 @@ function HowToCleanPowerSection ({
         That will solve another {powerPrcnt}% of the problem.
       </p>
 
-      <div className="mt-5 d-flex justify-content-center">
+      <div className="mt-5 d-flex justify-content-center d-block d-xl-none">
         <SingleBarChart
           emissionsData={latestEmissions}
           greenKeys={[ 'buildings', 'transportation', 'dirty_power' ]} />
