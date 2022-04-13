c8ef53025c295472c2e04a386d967913083efcc2
Derek Eder <derek.eder@gmail.com>
2022-04-13 17:52:49 -0500

    better power plant data key names, tidy up

--- data/scripts/get_power_plants.py
+++ data/scripts/get_power_plants.py
@@ -5,36 +5,9 @@ from utils import get_and_clean_csv, json_data_builder
 
 if __name__ == '__main__':
     path_to_data = sys.argv[1]
-    cols_to_keep = ["Plant primary coal/oil/gas/ other fossil fuel category","state", "Plant name", "Utility name", "Plant county name", "Plant nameplate capacity (MW)"]
+    cols_to_keep = ["fossil_fuel_category","state", "plant_name", "utility_name", "county", "capacity_mw"]
     power_plants = get_and_clean_csv(path_to_data, state_col="state", cols_to_keep=cols_to_keep)
 
     output = json.dumps(json_data_builder(power_plants, 'power_plants', is_array=False))
 
     print(output)
-
-
-# [
-#   {
-#     "state": "illinois",
-#     "power_plants": [
-#       {
-#         "type": "coal",
-#         "plants": [
-#           {
-#             "name": "E D Edwards",
-#             "utility_name": "Will County Energy LLC",
-#             "county_name": "Will",
-#             "power_capacity": "1,268.80"
-#           },
-#           ...
-#         ]
-#       },
-#       {
-#           "type": "gas",
-#           "plants": [
-#             ...
-#           ]
-#       }
-#     ]
-#   },
-# ]
