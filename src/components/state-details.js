c0480df8b6dbdc018d0df51c8b581b8de2df852d
Derek Eder <derek.eder@gmail.com>
2022-04-13 23:50:09 -0500

    === all the way

--- src/components/state-details.js
+++ src/components/state-details.js
@@ -90,9 +90,9 @@ const StateDetailsPage = ({location, data}) => {
 
   // #### POWER PLANTS ####
   const power_plants = data.allPowerPlantsJson.edges[0].node.power_plants
-  const coal_plants = power_plants.filter(plant => plant.fossil_fuel_category == "COAL")
-  const gas_plants = power_plants.filter(plant => plant.fossil_fuel_category == "GAS")
-  const oil_plants = power_plants.filter(plant => plant.fossil_fuel_category == "OIL")
+  const coal_plants = power_plants.filter(plant => plant.fossil_fuel_category === "COAL")
+  const gas_plants = power_plants.filter(plant => plant.fossil_fuel_category === "GAS")
+  const oil_plants = power_plants.filter(plant => plant.fossil_fuel_category === "OIL")
 
   // logging the data for debug
   console.log("coal_plants", coal_plants)
