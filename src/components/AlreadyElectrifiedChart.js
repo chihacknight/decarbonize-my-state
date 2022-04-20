35f13fae2335daf18d1b7ba6a25ec348d434f841
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-04-20 17:28:01 -0500

    Lint + Add back label

--- src/components/AlreadyElectrifiedChart.js
+++ src/components/AlreadyElectrifiedChart.js
@@ -3,15 +3,12 @@ import React from 'react'
 const electrifiedColor = "rgb(163, 215, 164)"
 const fossilColor = "rgb(255, 87, 34)"
 const AlreadyElectrifiedChart = ({ label, electrifiedPct, fossilPct }) => { //otherPct
-  const formattedData = {
-
-  }
   if (electrifiedPct !== 0 || fossilPct !== 0) {
     return (
       <svg width={"100%"} height="50px" viewBox="0 0 300 15" xmlns="http://www.w3.org/2000/svg" style={{ margin: '.5em 0 0 0' }}>
         <rect x={0} y={'50%'} width={`${electrifiedPct}%`} height="50%" fill={electrifiedColor} />
         <rect x={`${electrifiedPct}%`} y={'50%'} width={`${fossilPct}%`} height="50%" fill={fossilColor} />
-        <text x={0} y={'35%'} fontSize="10px">Electrified</text>
+        <text x={0} y={'35%'} fontSize="10px">{label} Electrified</text>
         <text x={'100%'} y={'35%'} fontSize="10px" textAnchor="end">Not yet</text>
       </svg>
     )
