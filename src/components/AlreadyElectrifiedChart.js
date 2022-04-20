4f6ab7caba3a5378ea7f5b385f7f840a27ee868a
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-04-20 17:36:56 -0500

    ARIA and accessibility

--- src/components/AlreadyElectrifiedChart.js
+++ src/components/AlreadyElectrifiedChart.js
@@ -5,7 +5,21 @@ const fossilColor = "rgb(255, 87, 34)"
 const AlreadyElectrifiedChart = ({ label, electrifiedPct, fossilPct }) => { //otherPct
   if (electrifiedPct !== 0 || fossilPct !== 0) {
     return (
-      <svg width={"100%"} height="50px" viewBox="0 0 300 15" xmlns="http://www.w3.org/2000/svg" style={{ margin: '.5em 0 0 0' }}>
+      <svg
+        width={"100%"}
+        height="50px"
+        viewBox="0 0 300 15"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ margin: '.5em 0 0 0' }}
+        aria-labelledby={`${label}-title ${label}-description`}
+        role="img"
+      >
+        <title id={`${label}-title`}>Percent of {label} electrified</title>
+        <desc id={`${label}-description`}>
+          A chart showing the share of {label} that have already been electrified and still based in fossil fuel.
+          {electrifiedPct}% have been electrified, and the remaining {fossilPct}% are fossil fuel based.
+        </desc>
+        
         <rect x={0} y={'50%'} width={`${electrifiedPct}%`} height="50%" fill={electrifiedColor} />
         <rect x={`${electrifiedPct}%`} y={'50%'} width={`${fossilPct}%`} height="50%" fill={fossilColor} />
         <text x={0} y={'35%'} fontSize="10px">{label} Electrified</text>
