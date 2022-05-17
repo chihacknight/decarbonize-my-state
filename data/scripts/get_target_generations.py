ccaa2d5bdecbc439ac90ba047ebd850098e7cf88
Sean Watland <seanjwatland@gmail.com>
2022-05-17 22:48:10 +0200

    updates to data

--- data/scripts/get_target_generations.py
+++ data/scripts/get_target_generations.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     path_to_data = sys.argv[1]
     # ,nrelRes,nrelCom,resNonElHeating,comNonElHeating,resNonElWaterHeating,resNonElWaterHeating,resNonElRange,pctResNonElHeating,pctComNonElHeating,pctResNonElWaterHeating,pctComNonElWaterHeating,pctResNonElRange
     us_target_generations = get_and_clean_csv(path_to_data, state_col="state")
-    output = json.dumps(json_data_builder(us_target_generations, 'target_generations', is_array=False))
+    output = json.dumps(json_data_builder(us_target_generations, is_array=True, array_key='targetBuilds'))
 
     print(output)
