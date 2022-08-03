bd9762d8f269d22d2a265a1c1941514e06c9ae02
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2022-08-03 18:01:34 -0500

    ranked each states emissions

--- src/pages/social-card.js
+++ src/pages/social-card.js
@@ -25,10 +25,57 @@ const SocialCardPage = ({data}) => {
   let currentState = params.get("state")
   let emissionsData = data.allEmissionsJson.edges
     .find(entry => entry.node.state === currentState)
-  {console.log(emissionsData)}
+  {console.log(data)}
 
-  // place info and string
-  // clean up title as needed
+  var eachStateRecentEmissions = []
+  var counter = 0
+  var emitterSuffix = 'th'
+
+  for (const stateData of data.allEmissionsJson.edges)
+  {
+    var tempState = stateData.node.state
+    var stateTotalEmissions = 0
+
+    if (tempState == 'united_states')
+    {
+      continue
+    }
+        
+    var mostRecentEmissions = stateData.node.emissionsByYear[stateData.node.emissionsByYear.length-1]
+    for (const [k,v] of Object.entries(mostRecentEmissions))
+    {
+      stateTotalEmissions += v
+    }
+
+        
+    eachStateRecentEmissions[counter] = [tempState, stateTotalEmissions, counter]
+    counter += 1
+  }
+    
+  eachStateRecentEmissions.sort((a,b) => b[1]-a[1])
+
+  for(var i=0;i<eachStateRecentEmissions.length;i++)
+  {
+    eachStateRecentEmissions[i][2] = i
+  }
+
+  let statePosInArr = eachStateRecentEmissions
+    .find(entry => entry[0]===currentState)
+
+  var finalDigitOfStatePos = statePosInArr[2]%10
+  if(finalDigitOfStatePos+1 == 1)
+  {
+    emitterSuffix = 'st'
+  }
+  else if (finalDigitOfStatePos+1==2)
+  {
+    emitterSuffix = 'nd'
+  }
+  else if (finalDigitOfStatePos+1==3)
+  {
+    emitterSuffix = 'rd'
+  }
+
   const placeTitle = slugToTitle(currentState)
   const stateFaceClass = currentState.toLowerCase().replaceAll(" ", "-")
 
@@ -41,9 +88,7 @@ const SocialCardPage = ({data}) => {
         style={{height: '418px', width:'800px'}}
       >
  
-        <div className="d-flex align-items-center col-6"
-            // style={{border: 'solid 1px red'}}
-        >
+        <div className="d-flex align-items-center col-6">
           <span
             className={"text-left display-3 sf-" + stateFaceClass}
             aria-hidden="true"
@@ -53,28 +98,27 @@ const SocialCardPage = ({data}) => {
 
           <div>
             <h3 id="main" className= "d-flex align-items-center mr-4 mt-0 mb-0" >
-                <span className="title font-weight-bold h4 mb-0"
-                style={{fontSize:'3.5rem'}}
-                >{placeTitle}</span>
+              <span className="title font-weight-bold h4 mb-0"
+                // style={{fontSize:(28/placeTitle.length)+'rem'}}
+                style={{fontSize:3.5+'rem'}}
+              >{placeTitle}</span>
             </h3>
 
-            <h4>3rd Highest Emitter</h4>
+            <h4>{(statePosInArr[2]+1)+emitterSuffix} Highest Emitter</h4>
           </div>
         </div>
 
-        <div className="col-6 d-block d-xl-none ml-2 justify-content-end"
-            // style={{border: 'solid 1px blue'}}
+        <div className="col-6 d-block d-xl-none ml-2 justify-content-end">
 
-        >
-            <p
+          <p
             className="text-right pt-2"
             style={{fontSize:'15px'}}
-            >CO2 Equivalent Emissions in {placeTitle} by Source</p>
-            <div className="justify-content-end ml-5 pl-5"            >
+          >CO2 Equivalent Emissions in {placeTitle} by Source</p>
+          <div className="justify-content-end ml-5 pl-5"            >
 
-                <SingleBarChart
-                    emissionsData={nodeData[emissionsData.node.emissionsByYear.length-1]}
-                    socialCardView={true}
+            <SingleBarChart
+              emissionsData={nodeData[emissionsData.node.emissionsByYear.length-1]}
+              socialCardView={true}
             />
           </div>
         </div>
