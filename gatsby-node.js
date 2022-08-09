c71ed52b8e5664ecb3adec0d5803f32e7e81f33f
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2022-08-09 18:01:24 -0500

    Fixed clipping issues for most states
    Note: before pushing this to main, we will need to change line 85 to ensure it has the correct base url

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,121 +1,116 @@
 // Based on Gatsby docs at https://www.gatsbyjs.com/docs/creating-and-modifying-pages/
-const path = require("path")
-const puppeteer = require('puppeteer')
-const fs = require('fs')
-const { siteMetadata } = require("./gatsby-config")
- 
+const path = require("path");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const { siteMetadata } = require("./gatsby-config");
+
 exports.createPages = async ({ graphql, actions, reporter, location }) => {
- const { createPage } = actions
+  const { createPage } = actions;
 
- var namesForSocialCardImage = []
- var waitingForReady = true
- 
- const placeNames = [
-   "alabama",
-   "alaska",
-   "arizona",
-   "arkansas",
-   "california",
-   "colorado",
-   "connecticut",
-   "delaware",
-   "district_of_columbia",
-   "florida",
-   "georgia",
-   "hawaii",
-   "idaho",
-   "illinois",
-   "indiana",
-   "iowa",
-   "kansas",
-   "kentucky",
-   "louisiana",
-   "maine",
-   "maryland",
-   "massachusetts",
-   "michigan",
-   "minnesota",
-   "mississippi",
-   "missouri",
-   "montana",
-   "nebraska",
-   "nevada",
-   "new_hampshire",
-   "new_jersey",
-   "new_mexico",
-   "new_york",
-   "north_carolina",
-   "north_dakota",
-   "ohio",
-   "oklahoma",
-   "oregon",
-   "pennsylvania",
-   "rhode_island",
-   "south_carolina",
-   "south_dakota",
-   "tennessee",
-   "texas",
-   "utah",
-   "vermont",
-   "virginia",
-   "washington",
-   "west_virginia",
-   "wisconsin",
-   "wyoming"
- ]
- 
- const StateDetailsTemplate = path.resolve(`src/components/state-details.js`)
- 
- async function captureScreenshot(name) {
-  waitingForReady = false;
-   // if screenshots directory is not exist then create one
-   if (!fs.existsSync("screenshots")) {
-     fs.mkdirSync("screenshots");
-   }
+  var namesForSocialCardImage = [];
+  var waitingForReady = true;
+
+  const placeNames = [
+    "alabama",
+    "alaska",
+    "arizona",
+    "arkansas",
+    "california",
+    "colorado",
+    "connecticut",
+    "delaware",
+    "district_of_columbia",
+    "florida",
+    "georgia",
+    "hawaii",
+    "idaho",
+    "illinois",
+    "indiana",
+    "iowa",
+    "kansas",
+    "kentucky",
+    "louisiana",
+    "maine",
+    "maryland",
+    "massachusetts",
+    "michigan",
+    "minnesota",
+    "mississippi",
+    "missouri",
+    "montana",
+    "nebraska",
+    "nevada",
+    "new_hampshire",
+    "new_jersey",
+    "new_mexico",
+    "new_york",
+    "north_carolina",
+    "north_dakota",
+    "ohio",
+    "oklahoma",
+    "oregon",
+    "pennsylvania",
+    "rhode_island",
+    "south_carolina",
+    "south_dakota",
+    "tennessee",
+    "texas",
+    "utah",
+    "vermont",
+    "virginia",
+    "washington",
+    "west_virginia",
+    "wisconsin",
+    "wyoming",
+  ];
+
+  const StateDetailsTemplate = path.resolve(`src/components/state-details.js`);
+
+  async function captureScreenshot(name) {
     let browser = null;
+    waitingForReady = false;
+    // if screenshots directory is not exist then create one
+    if (!fs.existsSync("screenshots")) {
+      fs.mkdirSync("screenshots");
+    }
     try {
-     // launch headless Chromium browser
-     browser = await puppeteer.launch({headless: false});
+      // launch headless Chromium browser
+      browser = await puppeteer.launch({ headless: false });
       // create new page object
-     const page = await browser.newPage();
+      const page = await browser.newPage();
       // set viewport width and height
-     await page.setViewport({ width: 800, height: 418 });
-     //no timeout time
-     await page.setDefaultNavigationTimeout(0);
+      await page.setViewport({ width: 800, height: 418 });
+      //no timeout time
+      page.setDefaultNavigationTimeout(0);
+      await page.goto("http://localhost:8000/social-card?state=" + name, {
+        waitUntil: "networkidle2",
+      });
 
-      await page.goto(siteMetadata.url+"social-card?state="+name, {waitUntil: "networkidle2"});
-
-        // capture screenshot and store it into screenshots directory.
-       await page.screenshot({ path: 'screenshots/'+name+'.jpeg' });
-       await browser.close();
+      // capture screenshot and store it into screenshots directory.
+      await page.screenshot({ path: "screenshots/" + name + ".jpeg" });
+      await browser.close();
       waitingForReady = true;
-
-     
-   } catch (err) {
-     console.log(`❌ Error: ${err.message}`);
-   }
- }
- 
- placeNames.forEach(name => {
-   const path = name
-   createPage({
-     path,
-     component: StateDetailsTemplate,
-     context: {
-       state: name
-     }
-   })
-
-   namesForSocialCardImage.push(name)
- })
-
- setTimeout(async function() {
-  for (const name of namesForSocialCardImage)
-  {
-    while(!waitingForReady){}
-    await captureScreenshot(name);
+    } catch (err) {
+      console.log(`❌ Error: ${err.message}`);
+    }
   }
- }, 3000)
-}
- 
 
+  placeNames.forEach((name) => {
+    const path = name;
+    createPage({
+      path,
+      component: StateDetailsTemplate,
+      context: {
+        state: name,
+      },
+    });
+    namesForSocialCardImage.push(name);
+  });
+
+  setTimeout(async function() {
+    for (const name of namesForSocialCardImage) {
+      while (!waitingForReady) {}
+      await captureScreenshot(name);
+    }
+  }, 3000);
+};
--- src/pages/social-card.js
+++ src/pages/social-card.js
@@ -1,171 +1,181 @@
-import React from "react"
-import SEO from "../components/seo"
-import SingleBarChart from "../components/singlebar"
-import { graphql } from "gatsby"
+import React from "react";
+import SEO from "../components/seo";
+import SingleBarChart from "../components/singlebar";
+import { graphql } from "gatsby";
 
 //TODO: move to shared file with state details
 const slugToTitle = (placeName) => {
-  const words = placeName.split("_")
-  
+  const words = placeName.split("_");
+
   for (let i = 0; i < words.length; i++) {
-    const word = words[i]
+    const word = words[i];
     if (word === "of") {
-      words[i] = word
+      words[i] = word;
     } else {
-      words[i] = word.charAt(0).toUpperCase() + word.slice(1)
+      words[i] = word.charAt(0).toUpperCase() + word.slice(1);
     }
   }
-  
-  return words.join(" ")
-}
 
-const SocialCardPage = ({location, data}) => {
+  return words.join(" ");
+};
+
+const SocialCardPage = ({ location, data }) => {
   //get the state data
-  let params = new URLSearchParams(location.search)
-  let currentState = params.get("state")
-  let emissionsData = data.allEmissionsJson.edges
-    .find(entry => entry.node.state === currentState)
+  let params = new URLSearchParams(location.search);
+  let currentState = params.get("state");
+  let emissionsData = data.allEmissionsJson.edges.find(
+    (entry) => entry.node.state === currentState
+  );
 
-
-  if(emissionsData == null)
-  {
-    emissionsData = data.allEmissionsJson.edges
-      .find(entry => entry.node.state === 'illinois')
-      currentState = 'illinois'
+  if (emissionsData == null) {
+    emissionsData = data.allEmissionsJson.edges.find(
+      (entry) => entry.node.state === "illinois"
+    );
+    currentState = "illinois";
   }
 
   //used for ranking the states
-  var eachStateRecentEmissions = []
-  var counter = 0
-  var emitterSuffix = 'th'
+  var eachStateRecentEmissions = [];
+  var counter = 0;
+  var emitterSuffix = "th";
 
   //go through each of the staes
-  for (const stateData of data.allEmissionsJson.edges)
-  {
-    var tempState = stateData.node.state
-    var stateTotalEmissions = 0
+  for (const stateData of data.allEmissionsJson.edges) {
+    var tempState = stateData.node.state;
+    var stateTotalEmissions = 0;
 
-    if (tempState === 'united_states')
-    {
-      continue
+    if (tempState === "united_states") {
+      continue;
     }
-        
+
     //sum all the emission categories to get total emissions for the recent year
-    var mostRecentEmissions = stateData.node.emissionsByYear[stateData.node.emissionsByYear.length-1]
-    for (const v of Object.values(mostRecentEmissions))
-    {
-      stateTotalEmissions += v
+    var mostRecentEmissions =
+      stateData.node.emissionsByYear[stateData.node.emissionsByYear.length - 1];
+    for (const v of Object.values(mostRecentEmissions)) {
+      stateTotalEmissions += v;
     }
 
-        
     //store in array of array containing state name, total emissions, and initial position
-    eachStateRecentEmissions[counter] = [tempState, stateTotalEmissions, counter]
-    counter += 1
+    eachStateRecentEmissions[counter] = [
+      tempState,
+      stateTotalEmissions,
+      counter,
+    ];
+    counter += 1;
   }
-    
+
   //sort array by emisisons
-  eachStateRecentEmissions.sort((a,b) => b[1]-a[1])
+  eachStateRecentEmissions.sort((a, b) => b[1] - a[1]);
 
   //change the position of each state to reflect their new position in sorted array
-  for(var i=0;i<eachStateRecentEmissions.length;i++)
-  {
-    eachStateRecentEmissions[i][2] = i
+  for (var i = 0; i < eachStateRecentEmissions.length; i++) {
+    eachStateRecentEmissions[i][2] = i;
   }
 
   //find the daa correspoinding to the current page
-  let statePosInArr = eachStateRecentEmissions
-    .find(entry => entry[0]===currentState)
+  let statePosInArr = eachStateRecentEmissions.find(
+    (entry) => entry[0] === currentState
+  );
 
   //to be used for specifically checks with /social-card/ which dont have a ?state={}
-  if(statePosInArr == null)
-  {
-    statePosInArr = eachStateRecentEmissions
-      .find(entry => entry[0]==='illinois')
+  if (statePosInArr == null) {
+    statePosInArr = eachStateRecentEmissions.find(
+      (entry) => entry[0] === "illinois"
+    );
   }
 
   //alter what the suffix of the number is
-  var finalDigitOfStatePos = statePosInArr[2]%10
-  if(finalDigitOfStatePos+1 === 1)
-  {
-    emitterSuffix = 'st'
-  }
-  else if (finalDigitOfStatePos+1===2)
-  {
-    emitterSuffix = 'nd'
-  }
-  else if (finalDigitOfStatePos+1===3)
-  {
-    emitterSuffix = 'rd'
+  var finalDigitOfStatePos = statePosInArr[2] % 10;
+  if (finalDigitOfStatePos + 1 === 1) {
+    emitterSuffix = "st";
+  } else if (finalDigitOfStatePos + 1 === 2) {
+    emitterSuffix = "nd";
+  } else if (finalDigitOfStatePos + 1 === 3) {
+    emitterSuffix = "rd";
   }
 
-  const placeTitle = slugToTitle(currentState)
-  const stateFaceClass = currentState.toLowerCase().replaceAll(" ", "-")
+  const placeTitle = slugToTitle(currentState);
+  const stateFaceClass = currentState.toLowerCase().replaceAll(" ", "-");
 
-  let nodeData = emissionsData.node.emissionsByYear
+  let nodeData = emissionsData.node.emissionsByYear;
 
-  var stateNameSize = 3.5
-  if(currentState === 'tennessee' || currentState === 'pennsylvania'
-   || currentState === 'washington' || currentState === 'oklahoma')
-  {
-        stateNameSize = 2.5
+  var stateNameSize = 3.5;
+  if (
+    currentState === "tennessee" ||
+    currentState === "pennsylvania" ||
+    currentState === "washington" ||
+    currentState === "oklahoma" ||
+    currentState === "connecticut" ||
+    currentState === "maryland" ||
+    currentState === "north_carolina"
+  ) {
+    stateNameSize = 2.5;
+  }
+  if (currentState === "massachusetts") {
+    stateNameSize = 1.7;
   }
 
-  return(
-    <div className='social-card d-flex flex-column'>
+  return (
+    <div className="social-card d-flex flex-column">
       <SEO title="Social Card" />
-      <div className="d-flex justify-content-between mr-2"
-        style={{height: '418px', width:'800px'}}
+      <div
+        className="d-flex justify-content-between mr-2"
+        style={{ height: "418px", width: "800px" }}
       >
         {/*state name, image and emitter rank */}
         <div className="d-flex align-items-center col-6">
           <span
             className={"text-left display-3 sf-" + stateFaceClass}
             aria-hidden="true"
-            style={{fontSize: '200px'}}
-
+            style={{ fontSize: "200px" }}
           ></span>
 
-          <div>
-            <h3 id="main" className= "d-flex align-items-center mr-4 mt-0 mb-0" >
-              <span className="title font-weight-bold h4 mb-0"
-                style={{fontSize:stateNameSize+'rem'}}
-              >{placeTitle}</span>
+          <div style={{ flexWrap: "wrap", width: "50%" }}>
+            <h3 id="main" className="d-flex align-items-center mr-4 mt-0 mb-0">
+              <span
+                className="title font-weight-bold h4 mb-0"
+                style={{
+                  fontSize: stateNameSize + "rem",
+                }}
+              >
+                {placeTitle}
+              </span>
             </h3>
 
-            <h4>{(statePosInArr[2]+1)+emitterSuffix} Highest Emitter</h4>
+            <h4>{statePosInArr[2] + 1 + emitterSuffix} Highest Emitter</h4>
           </div>
         </div>
 
         <div className="col-6 d-block d-xl-none ml-2 justify-content-end">
           {/* state emissions */}
-          <p
-            className="text-right pt-2"
-            style={{fontSize:'15px'}}
-          >CO2 Equivalent Emissions in {placeTitle} by Source</p>
-          <div className="justify-content-end ml-5 pl-4"            >
-
+          <p className="text-right pt-2" style={{ fontSize: "15px" }}>
+            CO2 Equivalent Emissions in {placeTitle} by Source
+          </p>
+          <div className="justify-content-end ml-4 pl-4">
             <SingleBarChart
-              emissionsData={nodeData[emissionsData.node.emissionsByYear.length-1]}
+              emissionsData={
+                nodeData[emissionsData.node.emissionsByYear.length - 1]
+              }
               socialCardView={true}
             />
           </div>
         </div>
       </div>
-      <h2 className="p-2 m-0 text-left" 
-        style={{width:'800px'}}        
-      >Decarb My State</h2>
+      <h2 className="p-2 m-0 text-left" style={{ width: "800px" }}>
+        Decarb My State
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default SocialCardPage
+export default SocialCardPage;
 
 export const query = graphql`
   query SocialQuery {
     allEmissionsJson {
       edges {
         node {
-            state
+          state
           emissionsByYear {
             dirty_power
             buildings
@@ -177,4 +187,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
