7baf359514b276c8404394422bb22a74c5446599
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2022-09-06 20:37:12 -0500

    added terminology to header and styled a little bit

--- src/pages/terminology.js
+++ src/pages/terminology.js
@@ -9,58 +9,60 @@ const Terminology = () => {
       <SEO title="Terminology" />
       <div className="container">
         <h1 id="main" className="py-2">
-          Terminology
+          Terms to Know
         </h1>
 
-        <p>Decarbonize my state - Terminology</p>
-        <p>
-          Carbon - a nonmetallic chemical element with atomic number 6 that
-          readily forms compounds with many other elements and is a constituent
-          of organic compounds in all known living tissues&nbsp;
-        </p>
-        <p>
-          Carbon Dioxide - a molecule consisting of one carbon atom and two
-          oxygen atoms.&nbsp; A product of combustion, respiration and other
-          natural processes.&nbsp; Also a greenhouse gas in excessive
-          quantities.
-        </p>
-        <p>Decarbonize -&nbsp; to remove carbon from&nbsp;</p>
-        <p>
-          Electrification - the process of converting combustion engines,
-          heating and cooking appliances from fossil fuels (mainly gasoline and
-          natural gas) to electric.
-        </p>
-        <p>
-          Geothermal - The natural heat emanating from the Earth&rsquo;s core
-          being conveyed by either magma or water to the Earth&rsquo;s surface
-          where it can be converted to electricity or used to heat homes.
-        </p>
-        <p>
-          Greenhouse gasses (GHG) - any of various gaseous compounds (such as
-          carbon dioxide or ozone) that absorb infrared light, trapping&nbsp;
-          heat in the atmosphere, and contributes to the greenhouse effect
-        </p>
-        <p>
-          Greenhouse effect - the conversion of infrared light into heat.&nbsp;
-          Most noticed in closed automobiles on a sunny day.
-        </p>
-        <p>Megawatt - a million watts</p>
-        <p>
-          MTCO2e - <strong>Metric tons of carbon dioxide equivalent</strong> or
-          MTCO2e is the unit of measurement in this tool. The unit "CO2e"
-          represents an amount of a GHG whose atmospheric impact has been
-          standardized to that of one unit mass of carbon dioxide (CO2), based
-          on the global warming potential (GWP) of the gas.
-        </p>
-        <p>
-          Renewable Energy - Electricity which is produced by either wind, solar
-          or geothermal sources.
-        </p>
-        <p>
-          Watt -&nbsp; a unit of electric work&nbsp; or the power produced by a
-          current of one ampere across a potential difference of one volt{" "}
-          <strong>: </strong>&sup1;/₇₄₆ horsepower
-        </p>
+        <div className="mt-0">
+          <p>Decarbonize my state - Terminology</p>
+          <p>
+            Carbon - a nonmetallic chemical element with atomic number 6 that
+            readily forms compounds with many other elements and is a
+            constituent of organic compounds in all known living tissues&nbsp;
+          </p>
+          <p>
+            Carbon Dioxide - a molecule consisting of one carbon atom and two
+            oxygen atoms.&nbsp; A product of combustion, respiration and other
+            natural processes.&nbsp; Also a greenhouse gas in excessive
+            quantities.
+          </p>
+          <p>Decarbonize -&nbsp; to remove carbon from&nbsp;</p>
+          <p>
+            Electrification - the process of converting combustion engines,
+            heating and cooking appliances from fossil fuels (mainly gasoline
+            and natural gas) to electric.
+          </p>
+          <p>
+            Geothermal - The natural heat emanating from the Earth&rsquo;s core
+            being conveyed by either magma or water to the Earth&rsquo;s surface
+            where it can be converted to electricity or used to heat homes.
+          </p>
+          <p>
+            Greenhouse gasses (GHG) - any of various gaseous compounds (such as
+            carbon dioxide or ozone) that absorb infrared light, trapping&nbsp;
+            heat in the atmosphere, and contributes to the greenhouse effect
+          </p>
+          <p>
+            Greenhouse effect - the conversion of infrared light into
+            heat.&nbsp; Most noticed in closed automobiles on a sunny day.
+          </p>
+          <p>Megawatt - a million watts</p>
+          <p>
+            MTCO2e - <strong>Metric tons of carbon dioxide equivalent</strong>{" "}
+            or MTCO2e is the unit of measurement in this tool. The unit "CO2e"
+            represents an amount of a GHG whose atmospheric impact has been
+            standardized to that of one unit mass of carbon dioxide (CO2), based
+            on the global warming potential (GWP) of the gas.
+          </p>
+          <p>
+            Renewable Energy - Electricity which is produced by either wind,
+            solar or geothermal sources.
+          </p>
+          <p>
+            Watt -&nbsp; a unit of electric work&nbsp; or the power produced by
+            a current of one ampere across a potential difference of one volt{" "}
+            <strong>: </strong>&sup1;/₇₄₆ horsepower
+          </p>
+        </div>
       </div>
     </Layout>
   )
