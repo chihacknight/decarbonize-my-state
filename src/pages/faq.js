dd1ca2f80a44cd88d8df9c6b4cd197cd9ed6df8c
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2022-09-06 21:25:47 -0500

    Ran auto-format

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -8,7 +8,9 @@ const FAQ = () => {
     <Layout>
       <SEO title="FAQ" />
       <div className="container faq">
-        <h1 id="main" className="py-2">Frequently Asked Questions</h1>
+        <h1 id="main" className="py-2">
+          Frequently Asked Questions
+        </h1>
 
         <p>
           Do you have questions? We've compiled some of the most common ones we
@@ -20,49 +22,87 @@ const FAQ = () => {
         <h3>Do heat pumps work in colder climates?</h3>
 
         <p>
-          Google this question or ask a contractor and you might think the answer is no. But make no mistake, heat pumps absolutely work in cold climates. Not only that, but often they are the most energy efficient and cost effective solution available.
-          A few decades ago, most heat pumps stopped working when the temperature dropped below 20 or 30 degrees fahrenheit. By contrast, today’s heat pumps can run more efficiently than any other HVAC system all the way down to about -25 fahrenheit.
-          Just ask the millions of homeowners in Scandinavia. People in Norway, Finland and Sweden are installing heat pumps at a faster pace than anywhere else in Europe.
-          <a href="https://carbonswitch.com/do-heat-pumps-work-in-cold-weather/">Read more at CarbonSwitch</a>
+          Google this question or ask a contractor and you might think the
+          answer is no. But make no mistake, heat pumps absolutely work in cold
+          climates. Not only that, but often they are the most energy efficient
+          and cost effective solution available. A few decades ago, most heat
+          pumps stopped working when the temperature dropped below 20 or 30
+          degrees fahrenheit. By contrast, today’s heat pumps can run more
+          efficiently than any other HVAC system all the way down to about -25
+          fahrenheit. Just ask the millions of homeowners in Scandinavia. People
+          in Norway, Finland and Sweden are installing heat pumps at a faster
+          pace than anywhere else in Europe.
+          <a href="https://carbonswitch.com/do-heat-pumps-work-in-cold-weather/">
+            Read more at CarbonSwitch
+          </a>
         </p>
 
-        {
-          /**
-           * Electric Vehicle Section
-           */
-        }
+        {/**
+         * Electric Vehicle Section
+         */}
         <h2>Electric Vehicles (EVs)</h2>
 
-        <h3>Are EVs <em>really</em> lower emission, even with a dirty grid?</h3>
+        <h3>
+          Are EVs <em>really</em> lower emission, even with a dirty grid?
+        </h3>
 
         <p>
           Based off of information from
-          <a href="https://www.epa.gov/greenvehicles/electric-vehicle-myths#Myth1"> the EPA's page on electric vehicles</a>, here's
-          the short answer:
+          <a href="https://www.epa.gov/greenvehicles/electric-vehicle-myths#Myth1">
+            {" "}
+            the EPA's page on electric vehicles
+          </a>
+          , here's the short answer:
         </p>
 
         <p>
-          <strong>Electric vehicles almost always have a smaller carbon footprint than gasoline cars</strong>, even when accounting for the electricity used for charging, and even if your grid is very carbon intensive, like coal.
+          <strong>
+            Electric vehicles almost always have a smaller carbon footprint than
+            gasoline cars
+          </strong>
+          , even when accounting for the electricity used for charging, and even
+          if your grid is very carbon intensive, like coal.
         </p>
 
         <p>
-          The longer answer: Electric vehicles (EVs) have no tailpipe emissions. Generating the electricity used to charge EVs, however, may create carbon pollution. The amount varies widely based on how local power is generated, e.g., using coal or natural gas, which emit carbon pollution, versus renewable resources like wind or solar, which do not. Even accounting for these electricity emissions, research shows that an EV is typically responsible for lower levels of greenhouse gases (GHGs) than an average new gasoline car. As more renewable energy generation is brought online (or if you charge your car using your own solar panels) the total GHGs associated with EVs could be even lower.
+          The longer answer: Electric vehicles (EVs) have no tailpipe emissions.
+          Generating the electricity used to charge EVs, however, may create
+          carbon pollution. The amount varies widely based on how local power is
+          generated, e.g., using coal or natural gas, which emit carbon
+          pollution, versus renewable resources like wind or solar, which do
+          not. Even accounting for these electricity emissions, research shows
+          that an EV is typically responsible for lower levels of greenhouse
+          gases (GHGs) than an average new gasoline car. As more renewable
+          energy generation is brought online (or if you charge your car using
+          your own solar panels) the total GHGs associated with EVs could be
+          even lower.
         </p>
 
         <p>
-          If that sounds confusing, think about it this way: a gas car runs <em>completely</em> off of gasoline, and doesn't even burn it that efficiently. We already have some renewables (which have no emissions) and a large fossil fuel power plant can convert more of that chemical energy into usable electricity, rather than heat.
+          If that sounds confusing, think about it this way: a gas car runs{" "}
+          <em>completely</em> off of gasoline, and doesn't even burn it that
+          efficiently. We already have some renewables (which have no emissions)
+          and a large fossil fuel power plant can convert more of that chemical
+          energy into usable electricity, rather than heat.
         </p>
 
         <p>
           You can also
-          <a href="https://www.epa.gov/egrid/power-profiler"> learn more about electricity production in your area.
+          <a href="https://www.epa.gov/egrid/power-profiler">
+            {" "}
+            learn more about electricity production in your area.
           </a>
         </p>
 
         <p>
           <a href="https://www.fueleconomy.gov/feg/Find.do?action=bt2">
             EPA and DOE’s Beyond Tailpipe Emissions Calculator
-          </a> can help you estimate the greenhouse gas emissions associated with charging and driving an EV or a plug-in hybrid electric vehicle (PHEV) where you live. You can select an EV or PHEV model and type in your zip code to see the CO2 emissions and how they stack up against those associated with a gasoline car.
+          </a>{" "}
+          can help you estimate the greenhouse gas emissions associated with
+          charging and driving an EV or a plug-in hybrid electric vehicle (PHEV)
+          where you live. You can select an EV or PHEV model and type in your
+          zip code to see the CO2 emissions and how they stack up against those
+          associated with a gasoline car.
         </p>
 
         <h3>How long does it take to charge an EV?</h3>
@@ -74,7 +114,8 @@ const FAQ = () => {
         </p>
 
         <p>
-          Learn more from <a href="https://www.tomsguide.com/reference/ev-charging-explained-heres-all-the-different-charger-types">
+          Learn more from{" "}
+          <a href="https://www.tomsguide.com/reference/ev-charging-explained-heres-all-the-different-charger-types">
             Tom's Guide - EV Charging Explained
           </a>
         </p>
@@ -108,12 +149,14 @@ const FAQ = () => {
         <p>
           Learn more from{" "}
           <a href="https://www.nature.com/articles/d41586-021-02222-1">
-            Nature - Electric cars and batteries: how will the world produce enough?
+            Nature - Electric cars and batteries: how will the world produce
+            enough?
           </a>
         </p>
 
         <h3>
-          But are electric cars <em>really</em> the best climate transporation solution?
+          But are electric cars <em>really</em> the best climate transporation
+          solution?
         </h3>
 
         <p>
@@ -127,8 +170,8 @@ const FAQ = () => {
         </p>
 
         <p>
-          As an example, an ebike (let's say Gazelle Ultimate T10) may have
-          a 500 Wh battery (0.5 kWh) that gives it a 25 mile range, or 50 miles
+          As an example, an ebike (let's say Gazelle Ultimate T10) may have a
+          500 Wh battery (0.5 kWh) that gives it a 25 mile range, or 50 miles
           per kWh. Meanwhile the Tesla Model 3, one of the most efficient EVs
           out right now, gets around 4.2 miles per kWh, making the
           <strong>e-bike 12 times more energy efficient!</strong> This does not
@@ -141,7 +184,8 @@ const FAQ = () => {
         <p>
           Learn more at{" "}
           <a href="https://ourworldindata.org/travel-carbon-footprint">
-            Our World in Data - Which form of transport has the lowest carbon footprint
+            Our World in Data - Which form of transport has the lowest carbon
+            footprint
           </a>
         </p>
       </div>
