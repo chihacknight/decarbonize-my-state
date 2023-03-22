b1d71020effc76fefe303e0b4339fb3ee373a207
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-03-21 20:55:06 -0500

    added slug feature and saved columns

--- data/scripts/get_power_plants.py
+++ data/scripts/get_power_plants.py
@@ -7,13 +7,11 @@ from slugify import slugify
 if __name__ == '__main__':
     path_to_data = sys.argv[1]
     cols_to_keep = ["fossil_fuel_category","state", "plant_name", "utility_name", "county", "capacity_mw", "Plant annual CO2 equivalent emissions (tons)", "Plant annual NOx emissions (tons)", "Plant annual SO2 emissions (tons)", "Plant annual CO2 emissions (tons)", "Plant annual CH4 emissions (lbs)", "Plant annual N2O emissions (lbs)", "Plant annual net generation (MWh)", "Latitude", "Longitude"]
-    number_cols = ["capacity_mw", "Plant annual CO2 equivalent emissions (tons)", "Plant annual NOx emissions (tons)", "Plant annual SO2 emissions (tons)", "Plant annual CO2 emissions (tons)", "Plant annual CH4 emissions (lbs)", "Plant annual N2O emissions (lbs)", "Plant annual net generation (MWh)", "Latitude", "Longitude"]
+    number_cols = ["capacity_mw"]
     power_plants = get_and_clean_csv(path_to_data, state_col="state", cols_to_keep=cols_to_keep, number_cols=number_cols)
 
-    for p in power_plants:
-        p['slug'] = slugify(p['place_name'])
+    power_plants['slug'] = power_plants['plant_name'].apply(lambda value: slugify(value))
         
-
     output = json.dumps(json_data_builder(power_plants, 'power_plants', is_array=True, array_key="power_plants"))
 
     print(output)
