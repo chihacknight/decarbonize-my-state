37499789bc587d68c139358a2d1f799e07be275e
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2023-04-16 11:56:42 -0500

    Start improving power plant details styling

--- src/components/power-plant-detail.js
+++ src/components/power-plant-detail.js
@@ -4,10 +4,16 @@ import { graphql } from "gatsby"
 import Layout from "./layout"
 import SEO from "./seo"
 
+// Sim city power plant icons
 import OilPlantImg from "../images/oil-plant.png"
+import GasPlantImg from "../images/gas-plant.png"
+import CoalPlantImg from "../images/coal-plant.png"
+
 import NewTabIcon from "../components/new-tab-icon"
 
 const PowerPlantDetailPage = ({ pageContext, data }) => {
+  console.log('data', data);
+  const PowerPlantState = data.allPowerPlantsJson.edges[0].node.state;
   const PowerPlant = data.allPowerPlantsJson.edges[0].node.power_plants.find(
     plant => plant.slug === pageContext.powerPlantSlug
   )
@@ -20,52 +26,72 @@ const PowerPlantDetailPage = ({ pageContext, data }) => {
       <SEO title={PowerPlant.plant_name + " Power Plant | Decarb My State"} />
 
       <div className="power-plant-page">
-        <img src={OilPlantImg} alt="Oil power plant" />
-
         <div>
-          <h1 id="main">{PowerPlant.plant_name}</h1>
+          <div className="top-row d-flex align-items-center">
+            { PowerPlant.fossil_fuel_category.toLowerCase() === 'oil' &&
+              <img src={OilPlantImg} alt="Oil power plant" /> }
+            { PowerPlant.fossil_fuel_category.toLowerCase() === 'gas' &&
+              <img src={GasPlantImg} alt="Gas power plant" /> }
+            { PowerPlant.fossil_fuel_category.toLowerCase() === 'coal' &&
+              <img src={CoalPlantImg} alt="Coal power plant" /> }
 
-          <div className="fuel-category">
-            {PowerPlant.fossil_fuel_category.toLowerCase()} power plant
+            <div>
+              <h1 id="main" className="mb-0">{PowerPlant.plant_name}</h1>
+
+              <div className="h5 mb-0 font-weight-bold">
+                <span className="text-capitalize">
+                  {PowerPlant.fossil_fuel_category.toLowerCase()}
+                </span> Power Plant
+              </div>
+              <div>
+                {PowerPlant.county} County, <span className="text-capitalize">{PowerPlantState}</span>
+              </div>
+
+              <a
+                href={GoogleMapsLink}
+                href="https://communitysolar.energysage.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                View on Google Maps
+                <NewTabIcon />
+              </a>
+            </div>
           </div>
 
-          <a
-            href={GoogleMapsLink}
-            href="https://communitysolar.energysage.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            View on Google Maps
-            <NewTabIcon />
-          </a>
-
           <dl>
-            <dt>County:</dt>
-            <dd>{PowerPlant.county}</dd>
-
-            <dt>Capacity:</dt>
+            <dt>Capacity</dt>
             <dd>{PowerPlant.capacity_mw} Megawatts</dd>
 
-            <dt>Plant_annual_net_generation__MWh_</dt>
-            <dd>{PowerPlant.Plant_annual_net_generation__MWh_}</dd>
+            <dt>Annual Net Generation</dt>
+            <dd>
+              {PowerPlant.Plant_annual_net_generation__MWh_}
+              &nbsp;MWh (Megawatt Hours)
+            </dd>
 
-            <dt>Plant_annual_SO2_emissions__tons_</dt>
-            <dd>{PowerPlant.Plant_annual_SO2_emissions__tons_}</dd>
+            <dt>Annual CO<sub>2</sub>e emissions</dt>
+            <dd>
+              {PowerPlant.Plant_annual_CO2_equivalent_emissions__tons_}
+              &nbsp;metric tons CO<sub>2</sub> equivalent
+            </dd>
 
-            <dt>Plant_annual_NOx_emissions__tons_</dt>
-            <dd>{PowerPlant.Plant_annual_NOx_emissions__tons_}</dd>
+            <dt>Annual CO<sub>2</sub> emissions</dt>
+            <dd>
+              {PowerPlant.Plant_annual_CO2_emissions__tons_}
+              &nbsp;metrics tons CO<sub>2</sub>
+            </dd>
 
-            <dt>Plant_annual_N2O_emissions__lbs_</dt>
-            <dd>{PowerPlant.Plant_annual_N2O_emissions__lbs_}</dd>
+            <dt>Annual SO<sub>2</sub> (Sulfer Dioxide) emissions</dt>
+            <dd>{PowerPlant.Plant_annual_SO2_emissions__tons_} tons</dd>
 
-            <dt>Plant_annual_CH4_emissions__lbs_</dt>
-            <dd>{PowerPlant.Plant_annual_CH4_emissions__lbs_}</dd>
+            <dt>Annual NOx (Nitrogen Oxide) emissions</dt>
+            <dd>{PowerPlant.Plant_annual_NOx_emissions__tons_} tons</dd>
 
-            <dt>Plant_annual_CO2_emissions__tons_</dt>
-            <dd>{PowerPlant.Plant_annual_CO2_emissions__tons_}</dd>
+            <dt>Annual N<sub>2</sub>O (Nitrous Oxide) emissions</dt>
+            <dd>{PowerPlant.Plant_annual_N2O_emissions__lbs_} lbs</dd>
 
-            <dt>Plant_annual_CO2_equivalent_emissions__tons_</dt>
-            <dd>{PowerPlant.Plant_annual_CO2_equivalent_emissions__tons_}</dd>
+            <dt>Annual CH4 (Methane) emissions</dt>
+            <dd>{PowerPlant.Plant_annual_CH4_emissions__lbs_} lbs</dd>
           </dl>
         </div>
       </div>
@@ -82,6 +108,7 @@ export const query = graphql`
     ) {
       edges {
         node {
+          state
           power_plants {
             slug
             plant_name
